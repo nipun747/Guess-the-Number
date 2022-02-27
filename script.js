7650f6f06ed8207d223c4024cc089ca7e0bc4027
nipun <anipun747@gmail.com>
2022-02-27 22:00:59 +0600

    background change

--- script.js
+++ script.js
@@ -5,9 +5,9 @@
 // document.querySelector('.score').textContent=10
 // document.querySelector('.guess').value=30
 
-const secretNumber = Math.trunc(Math.random()*20+1);
+let secretNumber = Math.trunc(Math.random()*20+1);
 let score=20;
-document.querySelector('.number').textContent=secretNumber;
+
 
 document.querySelector('.check').addEventListener('click',function(){
     const guess=Number(document.querySelector('.guess').value);
@@ -16,6 +16,9 @@ document.querySelector('.check').addEventListener('click',function(){
         document.querySelector('.message').textContent='No Number';
     }else if(guess==secretNumber){
         document.querySelector('.message').textContent='Correct Number';
+        document.querySelector('.number').textContent=secretNumber;
+        document.querySelector('body').style.backgroundColor='green';
+        document.querySelector('.number').style.width='38rem';
     }
     else if(guess> secretNumber){
         if(score>1)
@@ -45,3 +48,15 @@ document.querySelector('.check').addEventListener('click',function(){
 
     }
 });
+document.querySelector('.again').addEventListener('click',function()
+{
+    score=20;
+    secretNumber = Math.trunc(Math.random()*20+1);
+    document.querySelector('.message').textContent='Start guessing....';
+    document.querySelector('.score').textContent=score;
+    document.querySelector('.number').textContent='?';
+    document.querySelector('.guess').value='';
+    document.querySelector('body').style.backgroundColor='#222';
+        document.querySelector('.number').style.width='15rem';
+
+})
